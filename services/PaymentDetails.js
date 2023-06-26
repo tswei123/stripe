893926a33c0d828148aa658f9f53d16476bc164f
Shao Wei <tohswei123@gmail.com>
2023-06-27 06:33:25 +0800

    fixed api call

--- services/PaymentDetails.js
+++ services/PaymentDetails.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 
 const paymentDetails = async (email) => {
     try {
-        const paymentResponse = await axios.get(`https://tic4902-stripe-backend-ca3a56671ba8.herokuapp.com/order/getIndividualServiceCart?email=${email}`);
+        const paymentResponse = await axios.get(`https://tic4902-backend-9892fd41cb23.herokuapp.com/api/order/getIndividualServiceCart?email=${email}`);
         return paymentResponse.data;
     } catch (error) {
         throw error;
