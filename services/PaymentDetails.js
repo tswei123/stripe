799cf06d90a3938501ace18b381e8ad6cdcdb80f
Shao Wei <tohswei123@gmail.com>
2023-06-27 06:30:17 +0800

    change paymend details

--- services/PaymentDetails.js
+++ services/PaymentDetails.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 
 const paymentDetails = async (email) => {
     try {
-        const paymentResponse = await axios.get(`${process.env.REACT_APP_SPRING_URL}/order/getIndividualServiceCart?email=${email}`);
+        const paymentResponse = await axios.get(`https://tic4902-stripe-backend-ca3a56671ba8.herokuapp.com/order/getIndividualServiceCart?email=${email}`);
         return paymentResponse.data;
     } catch (error) {
         throw error;
